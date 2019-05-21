8f60a23878c582a8abed3fd2fbe2a8fb9eb0f670
Giorgio Aquino <giorgio.aquino@abstract.it>
2019-05-21 15:47:48 +0200

    feat: add feed parser

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const micro = require("micro");
 const query = require("micro-query");
 var Feed = require("rss-to-json");
 
-module.exports = micro((req, res) => {
+module.exports = micro(async (req, res) => {
   const { url = "" } = query(req);
-  return url;
+
+  const rss = await new Promise(resolve => {
+    Feed.load(url, function(err, rss) {
+      resolve(rss);
+    });
+  });
+
+  return rss;
 });
