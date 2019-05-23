a4a2fbe060bc1e7f17bf699393272c3d0b02fb35
Giorgio Aquino <giorgio.aquino@abstract.it>
2019-05-23 09:27:11 +0200

    fix: change parse library

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const micro = require("micro");
 const query = require("micro-query");
 const cors = require("micro-cors")();
 
-var Feed = require("rss-to-json");
+let Parser = require("rss-parser");
+let parser = new Parser();
 
 const handle = async (req, res) => {
   const { url = "" } = query(req);
 
-  const rss = await new Promise(resolve => {
-    Feed.load(url, function(err, rss) {
-      resolve(rss);
-    });
-  });
-
-  return rss;
+  try {
+    return await parser.parseURL(url);
+  } catch (e) {
+    micro.send(res, 500, e.message);
+  }
 };
 
 module.exports = micro(cors(handle));
